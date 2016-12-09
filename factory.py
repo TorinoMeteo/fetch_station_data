1d965c3c4a9a69edae4a933a2ad718e2d789cea6
abidibo <abidibo@gmail.com>
2016-12-09 13:08:10 +0100

    some expl

--- factory.py
+++ factory.py
@@ -4,6 +4,9 @@ import importlib
 def parser_factory(type):
     """ Factory method to retrieve the parse class to be used with
         contents formatted as type
+
+        WTF! Why all the if conditions?
+        To avoid auto import of modules, security is serius business.
     """
     path = '.parsers.%s'
     if type == 'txt-wd':
