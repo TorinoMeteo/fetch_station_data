720f9ce0cb307d0d5905509ca5e0f4e4c6ff9f7f
abidibo <abidibo@gmail.com>
2016-12-09 14:09:25 +0100

    clean revisited

--- parsers/txtwd.py
+++ parsers/txtwd.py
@@ -7,16 +7,18 @@ from ..labels import DATA_LABELS as DL
 class TxtWdParser(Parser):
 
     data_map = {
-        4: DL['TEMP'],
+        4: (DL['TEMP'], 'temp'),
+        5: (DL['TEMP_MAX'], 'temp'),
+        7: (DL['TEMP_MIN'], 'temp'),
     }
 
     def parse(self, content):
         lines = content.split(os.linesep)
 
         data = {}
-        for k, l in self.data_map.iteritems():
+        for k, i in self.data_map.iteritems():
             value = lines[k]
-            value = getattr(self, '_clean_%s' % l.lower())(value)
-            data[l] = value
+            value = getattr(self, '_clean_%s' % i[1].lower())(value)
+            data[i[0]] = value
 
         return data
